03937b9d9c6c4ce7f0eebf80108357b9c083e4a5
kompellasriniyati <48817920+kompellasriniyati@users.noreply.github.com>
2019-05-06 12:26:54 -0800

    Third Commit

--- booksback/src/main/java/com/niit/booksback/dao/CategoryDAOImpl.java
+++ booksback/src/main/java/com/niit/booksback/dao/CategoryDAOImpl.java
@@ -45,7 +45,7 @@ public class CategoryDAOImpl implements CategoryDAO {
 		catch(Exception e)
 		{
 			System.out.println(e.getMessage());
-			System.out.println("Not updated");
+			//System.out.println("Not updated");
 			return false;
 		}
 	}
